e11ac8df840a7c1360348a2ed2fac9803026452b
Divya <divyapallikonda259@gmail.com>
2025-04-10 00:04:20 +0530

    Update Account.java

--- src/main/java/com/example/banking_transaction_servicemanagement/dto/Account.java
+++ src/main/java/com/example/banking_transaction_servicemanagement/dto/Account.java
@@ -4,71 +4,79 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.math.BigDecimal;
 
 @Entity
 public class Account {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    
-	private int accId;
+    private int accId;
 
     private String accountHolderName;
     private String email;
     private String phoneNumber;
     private String accountType; // e.g., "Savings", "Current"
-    private Double balance;
+    private BigDecimal balance;
     private Boolean active = true;
-	public int getAccId() {
-		return accId;
-	}
 
-	public void setAccId(int accId) {
-		this.accId = accId;
-	}
+    public int getAccId() {
+        return accId;
+    }
 
-	public void setAccountId(int accountId) {
-		this.accId = accountId;
-	}
-	public String getAccountHolderName() {
-		return accountHolderName;
-	}
-	public void setAccountHolderName(String accountHolderName) {
-		this.accountHolderName = accountHolderName;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	public String getAccountType() {
-		return accountType;
-	}
-	public void setAccountType(String accountType) {
-		this.accountType = accountType;
-	}
-	public Double getBalance() {
-		return balance;
-	}
-	public void setBalance(Double balance) {
-		this.balance = balance;
-	}
-	public Boolean getActive() {
-		return active;
-	}
-	public void setActive(Boolean active) {
-		this.active = active;
-	}
-	public BigDecimal getBalance() {
-    return this.balance;
-}
-	
+    public void setAccId(int accId) {
+        this.accId = accId;
+    }
 
+    public void setAccountId(int accountId) {
+        this.accId = accountId;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public void setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
 }

46b8b53766a469fad717fa7cfd11439990c00b98
Divya <divyapallikonda259@gmail.com>
2025-04-10 00:46:23 +0530

    Update AccountRepo.java

--- src/main/java/com/example/banking_transaction_servicemanagement/repo/AccountRepo.java
+++ src/main/java/com/example/banking_transaction_servicemanagement/repo/AccountRepo.java
@@ -3,6 +3,10 @@ package com.example.banking_transaction_servicemanagement.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import com.example.banking_transaction_servicemanagement.dto.Account;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.repository.query.Param;
+
 
 import java.util.List;
 
@@ -23,7 +27,12 @@ public interface AccountRepo extends JpaRepository<Account, Integer> {
     // Custom method to find accounts with balance greater than a specified value
     @Query("SELECT a FROM Account a WHERE a.balance > ?1")
     List<Account> findAccountsWithBalanceGreaterThan(Double balance);
-    
+
+  @Modifying
+@Query("UPDATE Account a SET a.isActive = false WHERE a.accId = :accountId")
+void softDeleteById(@Param("accountId") int accountId);
+
+
 }
 
 
